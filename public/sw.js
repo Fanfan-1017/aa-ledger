60e355ef968d4ce34108fd431319b3f3ecbcf7cf
Fanfan-1017 <zhoushufanfan1998@gmail.com>
2026-07-27 16:50:17 +0800

    修复 Service Worker 缓存问题，网络优先策略

--- public/sw.js
+++ public/sw.js
@@ -1,13 +1,24 @@
-// Service Worker for PWA
-const CACHE = 'aa-ledger-v1';
-const URLS = ['/', '/index.html'];
+// Service Worker - 网络优先，确保始终最新
+const CACHE = 'aa-ledger-v3';
 
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(URLS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k))))
+  );
 });
 
 self.addEventListener('fetch', e => {
   e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request))
+    fetch(e.request).then(res => {
+      if (res.ok) {
+        const clone = res.clone();
+        caches.open(CACHE).then(c => c.put(e.request, clone));
+      }
+      return res;
+    }).catch(() => caches.match(e.request))
   );
 });
